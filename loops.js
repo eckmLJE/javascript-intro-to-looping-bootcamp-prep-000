285caf0c367eeb668d9336055d9e2c841a86db9a
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-08 15:51:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,8 @@ function forLoop(a) {
   for (var i = 1; i < 26; i++) {
     if (i === 1) {
       a.push("I am 1 strange loop");
+    } else {
+      a.push(`I am ${i} strange loops`);
     }
   }
 }
